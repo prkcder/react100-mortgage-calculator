0effa5b923509a4fd5115bc21df16b2a756d3b25
John Park <jhnprk12@gmail.com>
2019-05-28 22:44:16 -0700

    progress

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,8 +5,25 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
+      <title>Mortgage Calculator</title>
+        <h1>Mortgage Calculator</h1>
+        <input type='number' name='balance' placeholder='Loan Balance' />
+        <input type='number' name='rate' placeholder='Interest Rate %' step='0.1' />
+        <select name='term'>
+          <option value='15'>15</option>
+          <option value='30'>30</option>
+        </select>
+        <button type='button' name='submit'> Submit </button>
         {/* your JSX goes here */}
       </div>
     );
   }
 }
+
+    //   2) should have an input element with the name of "balance"
+    //   3) should have an input element with the name of "rate"
+    //   4) should have an input element with the name of "term"
+    //   5) should contain a button with the name of "submit"
+    // Integration
+    //   6) should display correct mortgage payment
+    //   7) should display correct mortgage payment
